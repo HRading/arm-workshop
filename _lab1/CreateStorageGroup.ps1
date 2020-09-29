3e913259f2ebc1766912ebba8d54ce2992b8a3fc
Henrik Rading <HRA@ctglobalservices.com>
2020-09-29 14:23:41 +0200

    Lab complete

---
+++ _lab1/CreateStorageGroup.ps1
@@ -0,0 +1,11 @@
+﻿Connect-AzAccount
+Get-AzSubscription | Out-GridView -Title 'Select subscription' -OutputMode Single | Select-AzSubscription
+
+$rg="hra-lab1"
+$job= 'job.' + ((Get-Date).ToUniversalTime()).tostring("MMddyy.HHmm")
+$template="C:\Github\arm-workshop\_lab1\azuredeploy.json"
+$parms="C:\Github\arm-workshop\_lab1\azuredeploy.parameters.json"
+
+
+$storageAccount = (New-AzResourceGroupDeployment -Name $job -TemplateFile $template -TemplateParameterFile $parms -ResourceGroupName $rg).Outputs.storageAccount.Value
+echo "Storage account $storageAccount has been created."
